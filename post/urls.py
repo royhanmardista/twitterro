c8c186016fd6fd7299ea6c18b4f9aa5d1bad5828
royhanmardista <royhanm23@gmail.com>
2020-01-20 23:39:47 +0700

    implementing class base view and mixins authorization

--- post/urls.py
+++ post/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView
 
 urlpatterns = [
-    path('', views.home, name='post-home'),
-    path('about/', views.about, name='post-about')
+    path('', PostListView.as_view(), name='post-home'),
+    path('about/', views.about, name='post-about'),
+    path('post/<int:pk>', PostDetailView.as_view(), name='post-detail'),
+    path('post/<int:pk>/update', PostUpdateView.as_view(), name='post-update'),
+    path('post/new/', PostCreateView.as_view(), name='post-create')
 ]
--- post/views.py
+++ post/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
+# using class base view (crud)
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
+# implementing login mixin for class base view decorators
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+
 
 
 # Create your views here.
@@ -10,5 +15,42 @@ def home(request) :
     }
     return render(request, "post/home.html", context)
 
+class PostListView(ListView) :
+    model = Post
+    template_name = 'post/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+class PostDetailView(DetailView) :
+    model = Post
+
+class PostCreateView(LoginRequiredMixin, CreateView) :
+    model = Post
+    fields = ['title', 'content']
+
+# overiding the author before save
+    def form_valid(self, form) :
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView) :
+    model = Post
+    fields = ['title', 'content']
+
+# overiding the author before save
+    def form_valid(self, form) :
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+    # implementing authorization
+    def test_func(self) :
+        post = self.get_object()
+        if (self.request.user == post.author) :
+            return True
+        else :
+            return False
+
 def about(request) :
     return render(request, "post/about.html", {'title' : 'about'})
+
