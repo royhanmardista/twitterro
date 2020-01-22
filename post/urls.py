ea84557f1153c49c74a1a1ff86f2784c9d86a9e6
royhanmardista <royhanm23@gmail.com>
2020-01-22 23:49:56 +0700

    ready to deploy

--- post/urls.py
+++ post/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 from . import views
-from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView
+from .views import UserPostListView, PostDeleteView ,PostListView, PostDetailView, PostCreateView, PostUpdateView
 
 urlpatterns = [
     path('', PostListView.as_view(), name='post-home'),
+    path('user/<str:username>', UserPostListView.as_view(), name='user-post'),
     path('about/', views.about, name='post-about'),
     path('post/<int:pk>', PostDetailView.as_view(), name='post-detail'),
     path('post/<int:pk>/update', PostUpdateView.as_view(), name='post-update'),
-    path('post/new/', PostCreateView.as_view(), name='post-create')
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/delete', PostDeleteView.as_view(), name='post-delete'),
+
 ]
--- post/views.py
+++ post/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Post
 # using class base view (crud)
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 # implementing login mixin for class base view decorators
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-
+from django.contrib.auth.models import User
 
 
 # Create your views here.
@@ -20,6 +20,17 @@ class PostListView(ListView) :
     template_name = 'post/home.html'
     context_object_name = 'posts'
     ordering = ['-date_posted']
+    paginate_by = 5
+
+class UserPostListView(ListView) :
+    model = Post
+    template_name = 'post/user_posts.html'
+    context_object_name = 'posts'
+    paginate_by = 5
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
 
 class PostDetailView(DetailView) :
     model = Post
@@ -42,6 +53,19 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView) :
     def form_valid(self, form) :
         form.instance.author = self.request.user
         return super().form_valid(form)
+    
+    # implementing authorization
+    def test_func(self) :
+        post = self.get_object()
+        if (self.request.user == post.author) :
+            return True
+        else :
+            return False
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView) :
+    model = Post
+    success_url = '/'
     
     # implementing authorization
     def test_func(self) :
